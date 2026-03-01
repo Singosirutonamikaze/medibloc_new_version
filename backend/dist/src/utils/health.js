b092ead3d28be583707dd07995ab57f75271b63b
yaodieudonnesingo60 <yaodieudonnesingo60@gmail.com>
2026-03-01 22:33:21 +0000

    chore: ignore dist and logs in backend

--- backend/dist/src/utils/health.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isServiceHealthy = isServiceHealthy;
-function isServiceHealthy() {
-    return true;
-}
-exports.default = isServiceHealthy;
